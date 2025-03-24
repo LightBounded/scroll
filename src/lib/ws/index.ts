112ed6c0dbda66d89cd6b106e955acec7b267253
Caleb Rivera <calebrossr@gmail.com>
2025-03-24 19:41:31 -0400

    add lobbyState message

--- src/lib/ws/index.ts
+++ src/lib/ws/index.ts
@@ -1,9 +1,9 @@
-export type WSGameMessage = 
-  | { event: 'playerJoin', player: string }
-  | { event: 'playerLeave', player: string }
-  | { event: 'playerSkip', skipper: string, skipped: string };
+export type WSGameMessage =
+	| { event: 'playerJoin'; player: string }
+	| { event: 'playerLeave'; player: string }
+	| { event: 'playerSkip'; skipper: string; skipped: string }
+	| { event: 'lobbyState'; players: string[] };
 
 export function sendWSGameMessage(ws: WebSocket, wsGameMessage: WSGameMessage) {
-  ws.send(JSON.stringify(wsGameMessage));
+	ws.send(JSON.stringify(wsGameMessage));
 }
-
